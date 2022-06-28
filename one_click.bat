f4420b1f0a4539ef8a19259513159baeee95e923
Astu04 <Astu04@users.noreply.github.com>
2022-06-28 20:46:07 +0200

    Create one_click.bat

---
+++ one_click.bat
@@ -0,0 +1,13 @@
+@echo off
+adb tcpip 5555
+for /F "tokens=*" %%A in (DeviceIP.txt) do (
+	adb connect %%A
+	adb -s %%A install -r "PokemodAtlas-Public-v22052602_2.apk"
+	adb -s %%A install -r "com.nianticlabs.pokemongo_0.237.0-2022050401_minAPI23(arm64-v8a)(nodpi)_apkmirror.com.apk"
+	adb -s %%A push atlas_config.json /data/local/tmp/atlas_config.json
+	adb -s %%A push emagisk.config /data/local/tmp/emagisk.config
+	adb -s %%A push first_install.sh /sdcard/first_install.sh
+	adb -s %%A shell "su -c '/system/bin/sh /sdcard/first_install.sh'"
+	adb -s %%A shell "rm /sdcard/first_install.sh"
+)
+pause
