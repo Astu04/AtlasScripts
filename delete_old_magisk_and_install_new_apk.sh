888d63434e3551bc8eb654f06c17041385e1ac95
Astu04 <Astu04@users.noreply.github.com>
2022-06-28 13:58:51 +0200

    Update delete_old_magisk_and_install_new_apk.sh

--- delete_old_magisk_and_install_new_apk.sh
+++ delete_old_magisk_and_install_new_apk.sh
@@ -9,11 +9,13 @@ magisk_ver="22.1"
 url_magisk="https://github.com/topjohnwu/Magisk/releases/download/v$magisk_ver/Magisk-v$magisk_ver.apk"
 useragent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'
 /system/bin/curl -s -k -L -o /sdcard/magisk.apk $url_magisk
-pm install -t -r /sdcard/magisk
+pm install -t -r /sdcard/magisk.apk
 rm /sdcard/magisk.apk 
 
 #Install eMagisk module
 /system/bin/curl -s -k -L -o /sdcard/eMagisk.zip https://github.com/tchavei/eMagisk/releases/download/latest/eMagisk-9.4.1.zip
 su -c 'magisk --install-module /sdcard/eMagisk.zip'
+/system/bin/curl -s -k -L -o /sdcard/ATVServices.sh https://raw.githubusercontent.com/tchavei/eMagisk/ad579b5b36f9c9a14a4b7936a4185c168f3afc5d/custom/ATVServices.sh
+su -c mv /sdcard/ATVServices.sh /data/adb/modules/emagisk/ATVServices.sh # The https fix pr isn't merged yet 2022-06-28
 
 reboot
