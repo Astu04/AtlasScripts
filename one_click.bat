a00ec08c6461d68d1a51836d970d73cfccd6f4f4
Astu04 <Astu04@users.noreply.github.com>
2022-06-29 13:45:01 +0200

    Update one_click.bat

--- one_click.bat
+++ one_click.bat
@@ -6,7 +6,9 @@ for /F "tokens=*" %%A in (DeviceIP.txt) do (
 	adb -s %%A install -r "com.nianticlabs.pokemongo_0.237.0-2022050401_minAPI23(arm64-v8a)(nodpi)_apkmirror.com.apk"
 	adb -s %%A push atlas_config.json /data/local/tmp/atlas_config.json
 	adb -s %%A push emagisk.config /data/local/tmp/emagisk.config
-	adb -s %%A push authorized_keys /sdcard/authorized_keys
+	if exist authorized_keys (
+		adb -s %%A push authorized_keys /sdcard/authorized_keys
+	)
 	adb -s %%A shell "/system/bin/curl -s -k -L https://raw.githubusercontent.com/Astu04/AtlasScripts/main/first_install.sh | su -c sh"
 )
 pause
